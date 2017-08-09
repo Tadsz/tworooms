3e98e2cf4703bf337f04cf608c516bc712c4eae6
= <jm@jmporchet.ch>
2017-08-09 20:28:18 +0200

    feat: add 7 new cards

--- imports/lib/specialSets.js
+++ imports/lib/specialSets.js
@@ -2,4 +2,15 @@ export const specialSets = [
   [5,6], // spies
   [7,8], // doctor+engineer
   [9,10], // tuesday knight+dr boom
+  [11,12], // bully+bully
+  [13,14], // cupid+eris
+];
+
+export const specialSingles = [
+  [
+    91, // gambler
+    92, // mime
+    93, // beholder
+    94, // born leader
+  ]
 ];
